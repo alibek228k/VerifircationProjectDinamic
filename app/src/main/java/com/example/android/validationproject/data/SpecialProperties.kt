f6a31174cde3541d1286c13864dfea69016d146f
Alibek <alibek228kz@gmail.com>
2022-05-10 11:09:47 +0600

    create a modal directory and configure it

---
+++ app/src/main/java/com/example/android/validationproject/data/SpecialProperties.kt
@@ -0,0 +1,5 @@
+package com.example.android.validationproject.data
+
+abstract class SpecialProperties (private var inputMaxLength: Int, private var inputMinLength:Int){
+
+}
---
+++ app/src/main/java/com/example/android/validationproject/modal/Form.kt
@@ -0,0 +1,5 @@
+package com.example.android.validationproject.modal
+
+class Form (var fields: List<Fields>)
+
+}
